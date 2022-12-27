cf3aceab7003c323148bdbc39b0e80dabf82c921
ibnuaviandi14 <113602897+ibnuaviandi14@users.noreply.github.com>
2022-12-27 09:25:57 +0700

    Add files via upload

---
+++ Task Sql Join/Revision Categories Vs Products.sql
@@ -0,0 +1,10 @@
+INSERT INTO `select categories.id, products.angka 
+from categories 
+left join products on products.id = categories.id` (id,angka) VALUES
+	 ('user_id','integer'),
+	 ('code','varchar'),
+	 ('name','varchar'),
+	 ('slug','varchar'),
+	 ('status','varchar'),
+	 ('photo','varchar'),
+	 ('description','longtest');
---
+++ Task Sql Join/Revision User Vs Products.sql
@@ -0,0 +1,11 @@
+INSERT INTO `select users.id, products.angka 
+from users 
+left join products on users.id = products.id` (id,angka) VALUES
+	 ('email',NULL),
+	 ('name','varchar'),
+	 ('password',NULL),
+	 ('address',NULL),
+	 ('role',NULL),
+	 ('status','varchar'),
+	 ('created_at',NULL),
+	 ('updated_at',NULL);
