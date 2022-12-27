5ab5db2b50fcb45aed84abb66a52f48ae1ed785a
ibnuaviandi14 <113602897+ibnuaviandi14@users.noreply.github.com>
2022-12-27 09:04:46 +0700

    Task SQL Syntax alias

---
+++ Task Sql Syntax/Revision Task Syntax 2.sql
@@ -0,0 +1,33 @@
+INSERT INTO users (name,phone,address) VALUES
+	 ('Andi',1234567891,'jl.1'),
+	 ('Anda',1234567892,'jl.2'),
+	 ('Ando',1234567893,'jl.3'),
+	 ('Andu',1234567894,'jl.4'),
+	 ('Ande',1234567895,'jl.5'),
+	 ('Aya',1234567896,'jl.6'),
+	 ('Ayi',1234567897,'jl.7'),
+	 ('Ayu',1234567898,'jl.8'),
+	 ('Aye',1234567899,'jl.9'),
+	 ('Ayo',1234567810,'jl.10');
+INSERT INTO users (name,phone,address) VALUES
+	 ('Apra',1234567811,'jl.11'),
+	 ('Apri',1234567812,'jl.12'),
+	 ('Apru',1234567813,'jl.13'),
+	 ('Apre',1234567814,'jl.14'),
+	 ('Apro',1234567815,'jl.15'),
+	 ('Andra',1234567816,'jl.16'),
+	 ('Andri',1234567817,'jl.17'),
+	 ('Andru',1234567818,'jl.18'),
+	 ('Andre',1234567819,'jl.19'),
+	 ('Andro',1234567820,'jl.20');
+INSERT INTO users (name,phone,address) VALUES
+	 ('Satria',1234567821,'jl.21'),
+	 ('Satrii',1234567822,'jl.22'),
+	 ('Satriu',1234567823,'jl.23'),
+	 ('Satrie',1234567824,'jl.24'),
+	 ('Satrio',1234567825,'jl.25'),
+	 ('Febra',1234567826,'jl.26'),
+	 ('Febri',1234567827,'jl.27'),
+	 ('Febru',1234567828,'jl.28'),
+	 ('Febre',1234567829,'jl.29'),
+	 ('Febro',1234567830,'jl.30');
