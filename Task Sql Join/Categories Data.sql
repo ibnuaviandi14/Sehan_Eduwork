563b42a2a8e20c2a8a384db39dd4eeee9a90de7d
unknown <Ibnu14aviandi@gmail.com>
2023-01-11 11:26:15 +0700

    Task Happy Day Shopping

--- Task Sql Join/Categories Data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `task sql join`.categories (id,angka) VALUES
-	 ('user_id','integer'),
-	 ('code','varchar'),
-	 ('name','varchar'),
-	 ('slug','varchar'),
-	 ('status','varchar'),
-	 ('photo','varchar'),
-	 ('description','text');
--- Task Sql Join/User Data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `task sql join`.users (id,angka) VALUES
-	 ('email','varchar'),
-	 ('name','varchar'),
-	 ('password','varchar'),
-	 ('address','varchar'),
-	 ('role','varchar'),
-	 ('status','varchar'),
-	 ('created_at','timestamp'),
-	 ('updated_at','timestamp');
